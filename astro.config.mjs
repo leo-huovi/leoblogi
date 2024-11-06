4568feffbb54eed8a2c678ea6e3e36b077105d8d
Leo <leo@localhost.localdomain>
2024-11-06 13:45:30 +0200

    deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,7 +32,7 @@ const oklchToHex = (str) => {
 // https://astro.build/config
 export default defineConfig({
   site: "https://leo-huovi.github.io",
-  base: "/leoblogi",
+  base: "/",
   trailingSlash: "always",
   integrations: [
     tailwind(),
