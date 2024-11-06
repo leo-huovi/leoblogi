23f9c37945c198f4ee0c94114d2535e99e3cb4fc
Leo <leo@localhost.localdomain>
2024-11-06 13:39:23 +0200

    url fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,8 +31,8 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "https://leo-huovi.github.io",
+  base: "/leoblogi",
   trailingSlash: "always",
   integrations: [
     tailwind(),
